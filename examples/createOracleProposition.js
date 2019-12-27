592f260819bba5df1289b7835d802ee480ae1f63
Joseph Goh <josephgoh7@gmail.com>
2019-12-27 14:35:10 +0800

    remove voter field in CreatePropositionMsg

--- examples/createOracleProposition.js
+++ examples/createOracleProposition.js
@@ -7,14 +7,13 @@ const mnemonics = require('../mnemonics.json')
 const privateKey = wallet.getPrivKeyFromMnemonic(mnemonics[1])
 const voterKey = wallet.getPrivKeyFromMnemonic(mnemonics[2])
 
-async function createOracleProposition() {
+async function createOracleProposition() { //
   const wallet = await Wallet.connect(privateKey)
   const voterWallet = await Wallet.connect(voterKey)
   const params = {
     OracleName: 'BTC_USD',
     Time: parseInt(Date.now() / 1000).toString(),
-    Data: '50000',
-    Voter: voterWallet.pubKeyBech32
+    Data: '50000'
   }
   api.createOracleProposition(wallet, params).then(console.log)
 }
