22f5d4985e0a27a9205e89fa5f4b8bbdece6e8e5
Raikonen <choozeyuan97@gmail.com>
2020-01-03 17:56:55 +0800

    Minor changes

--- src/lib/api/ws_wrapper.ts
+++ src/lib/api/ws_wrapper.ts
@@ -1,3 +1,25 @@
+export interface ICandlesticksGetParams {
+    market: string,
+    resolution: string,
+    from: string,
+    to: string
+}
+
+export interface ICandlesticksSubParams extends ISubParams {
+    resolution: string,
+}
+
+export interface ISubParams {
+    eventType: string,
+    market: string
+}
+
+interface IChannelId {
+    eventType: string,
+    market: string,
+    params: any
+}
+
 /*
     WsWrapper is a wrapper that helps to manage the socket connection with the server.
     Manages IDs for messages
@@ -17,140 +39,99 @@
     The other events will send messages through the event channel and will be handled by the application
 */
 
-export interface ICandlesticksGetParams {
-    market: string,
-    resolution: string,
-    from: string,
-    to: string
-  }
-  
-  export interface ISubParams {
-    eventType: string,
-    market: string
-  }
-  
-  export interface ICandlesticksSubParams extends ISubParams {
-    resolution: string,
-  }
-  
-  interface IChannelId {
-    eventType: string,
-    market: string,
-    params: any
-  }
-  
-  // TODO create interfaces for additional events
-  // TODO handle acknowledments for subscritpion and unsubscriptions (TBC)
-  // TODO implement unique ID System (Currently is just ++ from 1)
-  
-  export class WsWrapper {
+export class WsWrapper {
     serverWsUrl: string
-    isConnected: boolean = false
-    subscriptions: string[] = [] // List of subscribed channelIds
-    candlesticksSubscriptions: { [id: string]: string } = {} // subscribedIds to ChanneId mapping. For storing candlesticks subscription
-    candlesticksRequests: string[] = [] // List of requestIds for candlesticks 
-  
     socket: any
-    eventEmitter: any
     currId: number
+    isConnected: boolean = false
+    eventEmitter: any
     candlesticksOnHistoryCallback: any
     candlesticksOnRealtimeCallback: any
-  
+
+    subscriptions: string[] = [] // List of subscribed channelIds
+    candlesticksSubscriptions: { [id: string]: string } = {} // subscribeUID to channelIds mapping. For storing candlesticks subscription
+    candlesticksRequests: string[] = [] // List of request id number for candlesticks
+
     constructor(serverWsUrl: string, eventEmitter: any) {
         this.serverWsUrl = serverWsUrl
         this.eventEmitter = eventEmitter
         this.currId = 0
     }
-  
+
+    private handleGetCandlesticksResp(parsedMsg: any) {
+        if (parsedMsg.result.length) {
+            const bars: ReadonlyArray<object> =
+                parsedMsg.result.reverse().map((bar: any) => ({ // Reverse frontend or backend
+                    time: bar.time * 1000,
+                    close: bar.close,
+                    high: bar.high,
+                    open: bar.open,
+                    low: bar.low,
+                    volume: parseFloat(bar.volume) // Ignore quote volume
+                }))
+            this.candlesticksOnHistoryCallback(bars, { noData: false })
+        }
+        else {
+            this.candlesticksOnHistoryCallback(parsedMsg.result, { noData: true })
+        }
+    }
+
     public connect() {
         this.socket = new WebSocket(this.serverWsUrl)
-  
+
         // Config socket 
         this.socket.onopen = () => {
             this.isConnected = true
-            console.log('socket on')
+            console.log('socket is connected')
+            this.eventEmitter("connected")
         }
-  
-        /** Example message
-         *  id: 1
-         *  event: "candlesticks.swth_eth.1"
-         *  sequenceNumber: "100"
-         *  result: Candlestick obj
-         */
+
         this.socket.onmessage = (msg: any) => {
             try {
                 let parsedMsg: any
                 parsedMsg = JSON.parse(msg.data)
-                // Check if getCandlesticks response
-                if (this.candlesticksRequests.includes(parsedMsg.id)) {
-                    if (parsedMsg.result.length) {
-                        console.log(parsedMsg)
-                        const bars: ReadonlyArray<object> =
-                        parsedMsg.result.reverse().map((bar: any) => ({ // Reverse frontend or backend
-                          time: bar.time * 1000,
-                          close: bar.close,
-                          high: bar.high,
-                          open: bar.open,
-                          low: bar.low,
-                          volume: parseFloat(bar.volume)
-                        }))
-                        try {
-                          this.candlesticksOnHistoryCallback(bars, { noData: false })
-                        } catch (e) { console.log(e.message) }
-                    }
-                    else {
-                        this.candlesticksOnHistoryCallback(parsedMsg.result, { noData: true })
-                    }
+                console.log(parsedMsg)
+                if (parsedMsg.id && this.candlesticksRequests.includes(parsedMsg.id)) { // Check if get_candlestick resp
+                    this.handleGetCandlesticksResp(parsedMsg)
                 }
-                // Check if subscribeCandlestick acknowledgement
-                else if (parsedMsg.id && Object.keys(this.candlesticksSubscriptions).includes(parsedMsg.id)) {
-                    
-                } 
-                else { // Subscription messages
+                else if (parsedMsg.id && Object.keys(this.candlesticksSubscriptions).includes(parsedMsg.id)) { // Check if sub_candlestick resp
+                    const lastBar = { ...parsedMsg.result, time: parsedMsg.result.time * 1000 }
+                    this.candlesticksOnRealtimeCallback(lastBar)
+                }
+                else if (parsedMsg.channel && this.subscriptions.includes(parsedMsg.channel)) { // Check if other sub resp
                     let dataChannelId: IChannelId = this.parseChannelId(parsedMsg.channel)
-                    console.log(dataChannelId)
-                    if (dataChannelId.eventType === 'candlesticks') {
-                        const lastBar = { ...parsedMsg.result, time: parsedMsg.result.time * 1000 }
-                        console.log(lastBar)
-                        this.candlesticksOnRealtimeCallback(lastBar)
-                    }
-                    else {
-                        this.eventEmitter({...dataChannelId, ...parsedMsg}) // Pass it along into the original event channel
-                      }
+                    this.eventEmitter({ ...dataChannelId, ...parsedMsg }) // Pass it along into the original event channel
+                }
+                else { // Else is acknowledgement
+                    console.log("Acknowledged: " + parsedMsg.result)
                 }
             } catch (e) {
                 console.error(`Error parsing : ${msg.data}. Error: ${e.message}`)
             }
         }
-  
+
         this.socket.onclose = () => {
             this.isConnected = false
             console.log('socket off')
         }
     }
-  
+
     public disconnect() {
-        /*
-            // Unsubscribe to all the channels
-            let channelId: any
-            for (channelId in Object.values(this.subscriptions)) {
-                this.unsubscribe(channelId)
-            }
-        */
+        console.log("closing socket...")
         this.socket.close()
     }
-    
+
     public checkIsConnected(): boolean {
         return this.isConnected
     }
-  
+
     // Events are either "recent_trades", "books" or "candlesticks_realtime"
     public subscribe(params: ISubParams[]) { // List of params
-        console.log("Subscribing")
-        let Id: number = this.currId++
-        let channelIds = params.map((p: ISubParams) => this.generateChannelId(p))
+        let channelIds = params.map((p: ISubParams) => [p.eventType, p.market].join('.'))
+        let id: string = `${(this.currId++).toString()}.sub.${channelIds.join('.')}`
+        console.log("Subscribing to " + channelIds)
         const msg = JSON.stringify({
-            id: Id.toString(),
+            id: id,
             method: 'subscribe',
             params: { "channels": [...channelIds] }
         })
@@ -158,13 +139,13 @@ export interface ICandlesticksGetParams {
         // Add subscription
         this.subscriptions.push(...channelIds)
     }
-  
+
     public unsubscribe(params: ISubParams[]) {
-        console.log("Unsubscribing")
-        let Id: number = this.currId++
-        let channelIds: string[] = params.map((p: ISubParams) => this.generateChannelId(p))
+        let channelIds: string[] = params.map((p: ISubParams) => [p.eventType, p.market].join('.'))
+        let id: string = `${(this.currId++).toString()}.unsub.${channelIds.join('.')}`
+        console.log("Unsubscribing to " + channelIds)
         const msg = JSON.stringify({
-            id: Id.toString(),
+            id: id,
             method: 'unsubscribe',
             params: { "channels": [...channelIds] }
         })
@@ -172,71 +153,67 @@ export interface ICandlesticksGetParams {
         // Remove subscription
         this.subscriptions = this.subscriptions.filter((sub) => channelIds.includes(sub))
     }
-  
+
     public resubscribe(params: ISubParams[]) {
         this.unsubscribe(params)
+        setTimeout(() => { }, 1000)
         this.subscribe(params)
     }
-  
+
     /** Candlesticks functions */
-    
+
     public getCandlesticks(params: ICandlesticksGetParams, candlesticksOnHistoryCallback: any) {
-        let id: number = this.currId++
+        let id: string = `${(this.currId++).toString()}.get_candlesticks`
         const msg = JSON.stringify({
-            id: id.toString(),
+            id: id,
             method: 'get_candlesticks',
-            params: {...params}
+            params: { ...params }
         })
         this.candlesticksOnHistoryCallback = candlesticksOnHistoryCallback
-        console.log("Sending " + msg)
         // Add candlestick request
         this.candlesticksRequests.push(id.toString())
         this.socket.send(msg)
     }
-  
+
     public subscribeCandlesticks(params: ICandlesticksSubParams, candlesticksOnRealtimeCallback: any, subscribeUID: any) {
         this.candlesticksOnRealtimeCallback = candlesticksOnRealtimeCallback
-        let channelId: string = this.generateCandlesticksChannelId(params)
+        let channelId: string = ['candlesticks', params.market, params.resolution].join('.')
+        let id: string = `${subscribeUID}.sub.${channelId}`
         console.log("subscribing to " + channelId)
         const msg = JSON.stringify({
-            id: subscribeUID,
+            id: id,
             method: 'subscribe',
             params: { "channels": [channelId] }
         })
         // Add candlestick subscription
         this.candlesticksSubscriptions[subscribeUID] = channelId
+        console.log("Current candlestick subscription: " + this.candlesticksSubscriptions)
         this.socket.send(msg)
     }
-  
+
     public unsubscribeCandlesticks(subscribeUID: any) {
+        console.log("unsubscribing candlesticks")
         let channelId: string = this.candlesticksSubscriptions[subscribeUID] // Get channelId from local var
+        let id: string = `${subscribeUID}.sub.${channelId}`
+
         const msg = JSON.stringify({
-            id: subscribeUID,
+            id: id,
             method: 'unsubscribe',
             params: { "channels": [channelId] }
         })
         this.socket.send(msg)
         // Remove candlestick subscription
         delete this.candlesticksSubscriptions[subscribeUID]
+        console.log("Current candlestick subscription: " + this.candlesticksSubscriptions)
     }
-  
+
     private parseChannelId(rawChannelId: string): IChannelId {
         let splitChannelId = rawChannelId.split(".")
         let channelId: IChannelId = {
             eventType: splitChannelId[0],
             market: splitChannelId[1],
-            params: splitChannelId[2] // TODO fit params to event
+            params: splitChannelId[2]
         }
         return channelId
     }
-  
-    private generateChannelId(params: ISubParams): string {
-        return [params.eventType, params.market].join('.')
-    }
-  
-    private generateCandlesticksChannelId(params: ICandlesticksSubParams): string {
-        console.log(params.market)
-        return [params.eventType, params.market, params.resolution].join('.')
-    }
-  }
-  
+}
