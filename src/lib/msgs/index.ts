151d3599ead20fc136689e1dd3bfcfa44871ef08
Joseph Goh <josephgoh7@gmail.com>
2020-01-08 14:43:40 +0800

    [Oracles] remove msgs

--- src/lib/msgs/index.ts
+++ src/lib/msgs/index.ts
@@ -1,5 +1,4 @@
 export { MintTokenMsg } from './mints'
 export { CreateOrderMsg, CancelOrderMsg } from './orders'
 export { AddTokenMsg } from './tokens'
-export { CreateOracleMsg, CreatePropositionMsg } from './oracles'
 export { AddMarketMsg } from './markets'
