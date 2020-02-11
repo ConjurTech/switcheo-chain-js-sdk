537569461397741a4b3332e5925e6df27e04c6b0
Joseph Goh <josephgoh7@gmail.com>
2020-02-11 18:32:11 +0800

    update token to coin types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,8 +2,8 @@ export const CREATE_ORDER_MSG_TYPE = 'broker/CreateOrder'
 export const CANCEL_ORDER_MSG_TYPE = 'broker/CancelOrder'
 export const ADD_MARKET_MSG_TYPE = 'broker/AddMarket'
 
-export const MINT_TOKEN_MSG_TYPE = 'token/MintToken'
-export const CREATE_TOKEN_MSG_TYPE = 'token/CreateToken'
+export const MINT_TOKEN_MSG_TYPE = 'coin/MintToken'
+export const CREATE_TOKEN_MSG_TYPE = 'coin/CreateToken'
 
 export const CREATE_ORACLE_TYPE = 'oracle/CreateOracle'
 export const CREATE_VOTE_TYPE = 'oracle/CreateVote'
