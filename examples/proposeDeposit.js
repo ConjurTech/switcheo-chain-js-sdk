735ac29aff0a95dacc7172ebec6696d471c2dfe2
Joseph Goh <josephgoh7@gmail.com>
2020-02-11 10:49:56 +0800

    fix proposeDeposit example

--- examples/proposeDeposit.js
+++ examples/proposeDeposit.js
@@ -5,20 +5,19 @@ const { Wallet } = wallet
 const mnemonics = require('../mnemonics.json')
 
 const privateKey = wallet.getPrivKeyFromMnemonic(mnemonics[0])
-const privateKey2 = wallet.getPrivKeyFromMnemonic(mnemonics[2])
+// const privateKey2 = wallet.getPrivKeyFromMnemonic(mnemonics[2])
 
 async function proposeDeposit() {
   const wallet = await Wallet.connect(privateKey)
-  const wallet2 = await Wallet.connect(privateKey2)
+  // const wallet2 = await Wallet.connect(privateKey2)
   const params = {
-    Blockchain: "ETH",
-  	ChainID: "ETH",
-  	ExtAddress: "asdf",
-  	AccAddress: wallet2.pubKeyBech32,
-  	AssetID: "asdfg",
-  	Token: "eth",
-  	ExtTxID: "asdfgh",
-  	Amount: "50",
+    ExtAddress: "abc",
+    ExtTxID: "abc123",
+    AccAddress: wallet.pubKeyBech32,
+    Blockchain: "swth",
+  	ChainID: "1",
+  	AssetID: "iusd",
+  	Amount: "50000000000"
   }
 	api.proposeDeposit(wallet, params).then(console.log)
 }
