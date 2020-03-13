ef81fc98f1895fc189d9c15809d1d79d07350eff
junwei <chanjunwei@gmail.com>
2020-03-13 17:15:16 +0800

    merge conflicts

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 export { cancelOrder, cancelOrders, CancelOrderParams, createOrder, createOrders, CreateOrderParams, editOrder, editOrders, EditOrderParams } from './orders'
-=======
-export { cancelOrder, CancelOrderParams, createOrder, CreateOrderParams, editOrder, EditOrderParams } from './orders'
-export { setLeverage, SetLeverageParams } from './account'
->>>>>>> add setLeverage
 export { mintTokens, MintTokenRequest } from './mints'
 export { createToken, createTokens, CreateTokenMsg } from './tokens'
 export { createOracle, CreateOracleMsg, createProposition, CreatePropositionMsg } from './oracles'
@@ -11,6 +6,8 @@ export { proposeDeposit, ProposeDepositMsg, deposit, DepositParams } from './dep
 export { addMarket, addMarkets, AddMarketMsg } from './markets'
 export { createValidator, delegateTokens } from './staking'
 export { upgradeSWTH, upgradeSWTHs, UpgradeSwthMsg } from './upgrade_swth'
+export { setLeverage, SetLeverageParams } from './account'
+
 export {
   WsGetOrderHistoryParams,
   WsGetRecentTradesParams,
