c1a47b785900a3cd0c741b740ee07caf3fe1d5b4
John Wong <jwwwwong@gmail.com>
2020-08-17 17:08:25 +0800

    Fix check

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -201,7 +201,7 @@ export class Wallet {
   }
 
   public async watchDepositAddresses() {
-    if (this.network.NAME !== 'MAINNET') {
+    if (this.network.NAME !== 'mainnet') {
       return
     }
     this.watchNeoDepositAddress()
