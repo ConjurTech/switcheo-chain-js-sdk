9086e4e8ffcfabe83d6c249872906347ee932053
Henry Chua <henrychuahb@gmail.com>
2020-09-02 19:00:26 +0800

    Use tswth for testnet

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -77,7 +77,7 @@ export const NETWORK: NetworkInterface = {
     TARGET_PROXY_HASH: 'db8afcccebc026c6cae1d541b25f80a83b065c8a',
     FEE_ADDRESS: '989761fb0c0eb0c05605e849cae77d239f98ac7f',
     CHAIN_ID: 'switcheochain',
-    BECH32_PREFIX: 'swth',
+    BECH32_PREFIX: 'tswth',
   },
   MAINNET: {
     NAME: 'mainnet',
